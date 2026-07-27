0c33d9ec33662cec70dc271838b5310c2f271b6f
Mazen <mazenelkenany59@gmail.com>
2026-07-27 04:37:02 +0300

    fixed

--- LLMProvider.py
+++ LLMProvider.py
@@ -1,12 +1,12 @@
 import os
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_openai import ChatOpenAI
+
 load_dotenv()
 
 def get_llm():
     return ChatGoogleGenerativeAI(
-        model="gemini-2.5-flash",
+        model=os.getenv("GEMINI_MODEL"),
         google_api_key=os.getenv("GOOGLE_API_KEY"),
         temperature=0,
     )
